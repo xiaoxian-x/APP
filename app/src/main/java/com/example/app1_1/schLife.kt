f4abcf937ec640bf6c89e66b4d5f84fc356fc8be
xianxu <200210717@stu.hit.edu.cn>
2020-12-30 15:24:12 +0800

    1230byxxx

---
+++ app/src/main/java/com/example/app1_1/schLife.kt
@@ -0,0 +1,11 @@
+package com.example.app1_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class schLife : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sch_life)
+    }
+}
