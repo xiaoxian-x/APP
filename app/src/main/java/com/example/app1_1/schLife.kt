30a6fd3d68e09f051b5ff06651357edaddd41d16
xianxu <200210717@stu.hit.edu.cn>
2021-01-03 11:59:56 +0800

    1.3byxxx

--- app/src/main/java/com/example/app1_1/schLife.kt
+++ app/src/main/java/com/example/app1_1/schLife.kt
@@ -9,5 +9,21 @@ class schLife : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //引入一个布局：下方的三个按钮
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
